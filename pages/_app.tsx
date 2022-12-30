4edc852f37ab192483ddb123ce5fad1e861db16b
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-12-30 12:55:54 -0800

    Added supbase login modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,22 @@ import type { AppProps } from 'next/app';
 /** Auth0 */
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
+/** Supabase */
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import {
+  SessionContextProvider,
+  Session,
+  useUser,
+} from '@supabase/auth-helpers-react';
+
 /** MUI */
 import theme from '../theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import { useRouter } from 'next/router';
+import AuthModal from '../src/Components/AuthModal/AuthModal';
+import { AuthSession } from '@supabase/supabase-js';
 
 export const styles = {
   appBox: {
@@ -26,9 +37,33 @@ export const styles = {
   },
 } as const;
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const router = useRouter();
+  const [supabaseClient] = React.useState(() => createBrowserSupabaseClient());
+  const [session, setSesstion]: [
+    session: AuthSession | null,
+    setSession: Function
+  ] = React.useState(null);
+  const user = useUser();
+
+  React.useEffect(() => {
+    const getSession = () => {
+      supabaseClient.auth.getSession().then(({ data }) => setSesstion(data));
+    };
+
+    getSession();
+  }, []);
+
   return (
-    <UserProvider>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Box sx={styles.appBox}>
@@ -39,7 +74,8 @@ export default function App({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
           </Container>
         </Box>
+        <AuthModal />
       </ThemeProvider>
-    </UserProvider>
+    </SessionContextProvider>
   );
 }
